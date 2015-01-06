f9f144f7ad2565405f8ed92cd3034b302a9c9647
Chris Kaufmann <chriskaufmann@Chriss-MacBook-Air.local>
2015-01-05 22:49:26 -0600

    initial commit

---
+++ examples/ex.go
@@ -0,0 +1,49 @@
+package main
+
+import e "github.com/ChrisKaufmann/goutils"
+import "github.com/msbranco/goconfig"
+
+func main() {
+	print("e.Evenodd(1):"+e.Evenodd(1)+"\n")
+	print("e.Evenodd(0):"+e.Evenodd(0)+"\n")
+
+	print("e.Tostr(32):"+e.Tostr(32)+"\n")
+	print("e.Tostr(false):"+e.Tostr(false)+"\n")
+	print("e.Tostr(true):"+e.Tostr(true)+"\n")
+
+	print("e.Toint(\"0\"):"+e.Tostr(e.Toint("0"))+"\n")
+
+	print("\n")
+
+	print("arr := []int{1,2,3,4,5}\n")
+	arr := []int{1,2,3,4,5}
+	print("e.Shuffle(&arr):")
+	e.Shuffle(&arr)
+	for _,i := range arr {
+		print(i)
+		print(",")
+	}
+	print("\n")
+	print("\n")
+
+	print("ars := []string{\"a\",\"b\",\"c\",\"d\",\"e\"}\n")
+	ars := []string{"a","b","c","d","e"}
+	print("e.Shuffle(&ars):")
+	e.Shuffle(&ars)
+	for _,i := range ars {
+		print(i)
+		print(",")
+	}
+	print("\n")
+	print("\n")
+	print("e.GetHash(\"hello\"):"+e.GetHash("Hello")+"\n")
+	print("e.RandomString(32):"+e.RandomString(32)+"\n")
+
+	// use ew for error warning and ef for error fatal
+	// e.g.
+	_, err := goconfig.ReadConfigFile("config")
+	print("e.Ew(err):")
+	e.Ew(err)
+	print("\n")
+//	e.Ef(err)
+}
---
+++ goutils.go
@@ -0,0 +1,111 @@
+package goutils
+
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"io"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//puts path vars right into variables passed as params, until it runs out
+//ex: pathVars(r,"/entry/",&id,&todo,&val) // populates id, todo, and val
+func PathVars(r *http.Request, root string, vals ...*string) {
+	a := strings.Split(r.URL.Path[len(root):], "/")
+	for i := range vals {
+		if len(a) > i {
+			*vals[i] = a[i]
+		} else {
+			*vals[i] = ""
+		}
+	}
+}
+func Evenodd(i int) string {
+	if i%2 == 0 {
+		return "even"
+	}
+	return "odd"
+}
+func Tostr(i interface{}) string {
+	switch t := i.(type) {
+	case string:
+		return i.(string)
+	case int:
+		return strconv.Itoa(i.(int))
+	case bool:
+		return strconv.FormatBool(i.(bool))
+	case template.HTML:
+		return string(i.(template.HTML))
+	default:
+		t = t
+	}
+	return i.(string)
+}
+func Toint(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func Shuffle(data interface{})  {
+	switch t:= interface{}(data).(type){
+		case *[]int:
+			a := data.(*[]int)
+			data= Shufflei(*a)
+		case *[]string:
+			a := data.(*[]string)
+			data= Shuffles(*a)
+		default:
+			fmt.Printf("unexpected type %T", t) 
+	}
+}
+func Shuffles(slc []string) []string {
+	rand.Seed(time.Now().UnixNano())
+	N := len(slc)
+	for i :=0; i < N; i++ {
+		r := i + rand.Intn(N-i)
+		slc[r], slc[i] = slc[i], slc[r]
+	}
+	return slc
+}
+func Shufflei(slc []int) []int {
+	rand.Seed(time.Now().UnixNano())
+	N := len(slc)
+	for i := 0; i < N; i++ {
+		// choose index uniformly in [i, N-1]
+		r := i + rand.Intn(N-i)
+		slc[r], slc[i] = slc[i], slc[r]
+	}
+	return slc
+}
+func GetHash(s string) string {
+	h := sha1.New()
+	io.WriteString(h, s)
+	return fmt.Sprintf("%s", base64.URLEncoding.EncodeToString(h.Sum(nil)))
+}
+func RandomString(l int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(RandInt(65, 90))
+	}
+	return string(bytes)
+}
+func RandInt(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Intn(max-min)
+}
+func Ef(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+func Ew(err error) {
+	if err != nil {
+		print("WARN: "+err.Error())
+	}
+}
